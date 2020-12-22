ee048e89855374131d3b9085d56279746bd35c9f
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-22 16:19:58 +0000

    some changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import MessageList from "../components/message/MessageList";
+import Navigation from "../components/navigation/BottomNav";
+import SideNav from "../components/navigation/SIdeNav";
 import StoryList from "../components/story/StoryList";
 
 export default function Home() {
   return (
     <div className="container-fluid">
+      <SideNav />
       <div className="row">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
@@ -16,6 +20,12 @@ export default function Home() {
       <div className="row">
         <StoryList />
       </div>
+      <div className="row">
+        <MessageList />
       </div>
+      <div className="row">
+        <Navigation />
+      </div>
+    </div>
   )
 }
