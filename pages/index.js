4ae3f6efa03e3a55946bd46fc7661b83cb5ac534
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-22 10:10:06 +0000

    styles setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-import Head from 'next/head'
+import StoryList from "../components/story/StoryList";
 
 export default function Home() {
   return (
-    <div>Hello WOrld</div>
+    <div className="container-fluid">
+      <div className="row">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">All Chat</span>
+            <button className="btn bg-transparent">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+        </nav>
+      </div>
+      <div className="row">
+        <StoryList />
+      </div>
+      </div>
   )
 }
